ca8b830cb76f2cf571978fa91c5e16f0aa4defb7
Dmitry Tsybulenko <45.vilka@mail.ru>
2022-10-28 01:41:06 +0300

    Сделаны не до конца модели Сделаны опять же скорее всего не до конца ебанная templates Надеюсь скоро доделаю

---
+++ yatube/posts/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Post(models.Model):
+    text = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='post')
+
+    group = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='group')
+
+
+class Group(models.Model):
+    title = models.CharField()
+    slug = models.SlugField()
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.title}'
